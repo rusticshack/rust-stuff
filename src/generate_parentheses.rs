f6f9442a6d8702ada8654b53fb3a3c5afe2a9a37
mdavis <mdavis789@gmail.com>
2025-01-29 22:10:13 -0800

    Add implementation for generating well formed parentheses

---
+++ src/generate_parentheses.rs
@@ -0,0 +1,28 @@
+pub(crate) fn generate_parentheses(n: i32) -> Vec<String> {
+    let mut parens: Vec<String> = Vec::new();
+    let s: String = String::from("");
+    helper(n, 0, 0, s, &mut parens);
+    return parens;
+}
+
+fn helper(n: i32, l: i32, r: i32, s: String, parens: &mut Vec<String>) {
+    if l + r == 2 * n {
+        parens.push(s);
+    } else if l == r {
+        helper(n, l + 1, r, format!("{}{}", s, "("), parens);
+    } else {
+        if l < n {
+            helper(n, l + 1, r, format!("{}{}", s, "("), parens);
+        }
+        helper(n, l, r + 1, format!("{}{}", s, ")"), parens);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_generates_parentheses() {
+        assert_eq!(generate_parentheses(1), vec!["()"])
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod factorial;
 mod fibonnaci;
+mod generate_parentheses;
 
 fn main() {
     println!("{}", factorial::factorial(10));
     println!("{}", fibonnaci::fibonnaci(12));
+    println!("{:?}", generate_parentheses::generate_parentheses(5));
 }
