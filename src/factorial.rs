0fa4999972e9b7c5d60e1880c4b06124cbc5fc75
mdavis <mdavis789@gmail.com>
2025-02-02 12:38:27 -0800

    unit tests for factorial

--- src/factorial.rs
+++ src/factorial.rs
@@ -5,3 +5,16 @@ pub(crate) fn factorial(x: i32) -> i32 {
         x * factorial(x - 1)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_factorial() {
+        assert_eq!(factorial(1), 1);
+        assert_eq!(factorial(2), 2);
+        assert_eq!(factorial(3), 6);
+        assert_eq!(factorial(4), 24);
+        assert_eq!(factorial(10), 3628800);
+    }
+}
